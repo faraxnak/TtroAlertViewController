648aef61cade604e16709850475ea80e9d1afaf2
faraxnak <farid@farids-macbook-pro.local>
2016-12-28 18:40:17 +0330

    Initial Commit

---
+++ TtroAlertViewController/TtroAlertViewController.h
@@ -0,0 +1,19 @@
+//
+//  TtroAlertViewController.h
+//  TtroAlertViewController
+//
+//  Created by Farid on 12/28/16.
+//  Copyright © 2016 ParsPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TtroAlertViewController.
+FOUNDATION_EXPORT double TtroAlertViewControllerVersionNumber;
+
+//! Project version string for TtroAlertViewController.
+FOUNDATION_EXPORT const unsigned char TtroAlertViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TtroAlertViewController/PublicHeader.h>
+
+
